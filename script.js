8c455cc7ab70fbfa6670c6e548d478cb9fcbbdcc
saniaghaffar <saniaghaffar55@gmail.com>
2024-06-23 22:10:53 +0500

    complete

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ function squareRoot(){
           var s=Math.sqrt(r);
           document.getElementById("a").value=s;
 }
+function percentage(){
+          var p=document.getElementById("a").value;
+          var y=eval(p);
+          var d=y/100;
+          document.getElementById("a").value=d+"%";
+}
+function backSpace(){
+          var b=document.getElementById("a").value; 
+          var c=b.substring(0,b.length-1);
+          document.getElementById("a").value=c;
+}
+
